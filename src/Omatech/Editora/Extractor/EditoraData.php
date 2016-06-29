a6d0b2dc44ebc4c8a7f1f21cc1ce687715b5cecf
Agusti Pons <apons@omatech.com>
2016-06-29 15:37:52 +0200

    parent_args

--- src/Omatech/Editora/Extractor/EditoraData.php
+++ src/Omatech/Editora/Extractor/EditoraData.php
@@ -27,7 +27,7 @@ class EditoraData
 		
 		static function dump_current_args()
 		{
-				self::debug("lang=".self::$lang." limit=".self::$limit." id=".self::$id." class_id=".self::$class_id." debug=".self::$debug." preview=".self::$preview." preview_date=".self::$preview_date."\n");
+				echo("lang=".self::$lang." limit=".self::$limit." id=".self::$id." class_id=".self::$class_id." debug=".self::$debug." preview=".self::$preview." preview_date=".self::$preview_date."\n");
 		}
 		
 		
