c27826419d17292ea031f83ebce5f23063b8ba96
Agusti Pons <apons@omatech.com>
2016-07-04 15:37:17 +0200

    bug a getAllInstances array buit

--- samples/launch.php
+++ samples/launch.php
@@ -15,7 +15,7 @@ $params = [
 	, 'lang' => 'ca'
 	, 'debug' => false
 ];
-$show_metadata=true;
+$show_metadata=false;
 
  $query='query FetchHomeQuery ($id:Int, $lang:String, $debug:Boolean) {
   instance(id: $id, lang: $lang, debug: $debug) {
@@ -53,6 +53,7 @@ $show_metadata=true;
 
 EditoraData::set_connection($conn);
 $result=GraphQL::execute(EditoraSchema::build(), $query, null, $params);
+print_r($result);die;
 $ferretizer_result=Ferretizer::Ferretize($result['data'], $show_metadata);
 if ($ferretizer_result)
 {// todo ok 
