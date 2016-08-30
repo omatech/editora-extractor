b436ea34bc22c41be87178a41583bc3eb884a9b4
Agusti Pons <apons@omatech.com>
2016-08-30 16:29:59 +0200

    search query

--- samples/launch4_search.php
+++ samples/launch4_search.php
@@ -1,6 +1,15 @@
 <?php
 
-require '../../../autoload.php';
+$autoload_location=__DIR__.'/../vendor/autoload.php';
+if (is_file($autoload_location))
+{
+		
+}
+else
+{
+		$autoload_location='../../../autoload.php';	
+}
+require_once $autoload_location;
 require_once __DIR__.'/../conf/config.php';
 require_once __DIR__.'/../conf/bootstrap.php';
 
