94d3fb343a0343793104447dab19c504fee5f7ee
Agusti Pons <apons@omatech.com>
2016-09-01 12:08:13 +0200

    order i order_direction com a parametre del class

--- samples/launch2.php
+++ samples/launch2.php
@@ -22,7 +22,7 @@ $show_metadata=false;
 
  $query='query FetchClassQuery ($class_id:Int, $debug:Boolean, $lang:String) 
 {
-  class(class_id: $class_id, lang: $lang, debug: $debug, order:publishing_begins) 
+  class(class_id: $class_id, lang: $lang, debug: $debug, order:"publishing_begins") 
 	{
     class_id tag
 		instances
