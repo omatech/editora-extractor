7dc657e35757611ed0787131220a5da397276204
Agusti Pons <apons@omatech.com>
2016-09-13 09:13:57 +0200

    prova extraccio ids com llistes

--- samples/launch6_listofids.php
+++ samples/launch6_listofids.php
@@ -14,19 +14,26 @@ use Omatech\Editora\Extractor\Ferretizer;
 
 
 $params = [
-		'id' => '(784, 787, 100104)'
+		'query' => 'Brossa'
 	, 'lang' => 'ca'
-	, 'debug' => true
+	, 'preview' => true
+	, 'debug' => false
 ];
 $show_metadata=true;
 
- $query='query FetchListQuery ($id:String, $lang:String, $debug:Boolean) {
-  instance(id: $id, lang: $lang, debug: $debug) {
-		id nom_intern link publishing_begins status creation_date class_name class_tag class_id update_timestamp
-    all_values (filter: "small") {atri_tag text_val}
+ $query='query FetchSearchQuery ($query:String, $class_id:Int, $lang:String, $debug:Boolean, $preview:Boolean) 
+{
+  search(query: $query, class_id: $class_id, lang: $lang, debug: $debug, preview:$preview) 
+	{
+    query
+		instances
+		{
+				id nom_intern link publishing_begins status creation_date class_name class_tag class_id update_timestamp
+				all_values (filter: "fields:title|subtitle"){atri_tag text_val}				
+		}
   }
 }';
- 
+
 EditoraData::set_connection($conn);
 $result=GraphQL::execute(EditoraSchema::build(), $query, null, $params);
 $ferretizer_result=Ferretizer::Ferretize($result['data'], $show_metadata);
