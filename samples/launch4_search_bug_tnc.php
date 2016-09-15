08433b39a3963d6b74bc57e461d881c318e790fb
Agusti Pons <apons@omatech.com>
2016-09-15 20:16:08 +0200

    upgrade graphql to v0.7.1 launch4_search

--- samples/launch4_search_bug_tnc.php
+++ samples/launch4_search_bug_tnc.php
@@ -34,7 +34,7 @@ $query='query FetchSearchQuery ($query:String, $class_id:Int, $lang:String, $deb
 $show_metadata=true;
 
 EditoraData::set_connection($conn);
-$result=GraphQL::execute(EditoraSchema::build(), $query, null, $params);
+$result=GraphQL::execute(EditoraSchema::build(), $query, null, null, $params);
 $ferretizer_result=Ferretizer::Ferretize($result['data'], $show_metadata);
 if ($ferretizer_result)
 {// todo ok 
--- samples/launch5_searchsenseclass.php
+++ samples/launch5_searchsenseclass.php
@@ -35,7 +35,7 @@ $show_metadata=true;
 
  
 EditoraData::set_connection($conn);
-$result=GraphQL::execute(EditoraSchema::build(), $query, null, $params);
+$result=GraphQL::execute(EditoraSchema::build(), $query, null, null, $params);
 $ferretizer_result=Ferretizer::Ferretize($result['data'], $show_metadata);
 if ($ferretizer_result)
 {// todo ok 
