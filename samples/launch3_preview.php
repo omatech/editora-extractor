4204ba991aa81ef6f94bef14c076318359dd67c3
Agusti Pons <apons@omatech.com>
2016-09-16 16:39:09 +0200

    included limit in class and search

--- samples/launch3_preview.php
+++ samples/launch3_preview.php
@@ -38,7 +38,7 @@ $show_metadata=true;
 }';
 
 $extractor=new Extractor($conn);
-$res=$extractor->extract($query, $params, false);
+$res=$extractor->extract($query, $params, 'array', false);
 if ($res)
 {
 		echo "HA FUNCIONAT!!!";
