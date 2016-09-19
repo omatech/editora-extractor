7db335279a7cd9fc0cf4213d9f8551ec0c3eee82
Agusti Pons <apons@omatech.com>
2016-09-19 14:16:56 +0200

    $conn, object or array

---
+++ src/Omatech/Editora/Extractor/ExtractorServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Omatech\Editora\Extractor;
+
+use Illuminate\Support\ServiceProvider;
+use Omatech\Editora\Extractor\Extractor;
+
+class ExtractorServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->app['conn_doctrine'] = [
+            'dbname' => env('DB_DATABASE', 'forge'),
+            'dbuser' => env('DB_USERNAME', 'forge'),
+            'dbpass' => env('DB_PASSWORD', ''),
+            'dbhost' => env('DB_HOST', 'localhost'),
+        ];
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('extractor', function ($app) {
+            return new Extractor($app['conn_doctrine']);
+        });
+    }
+}
+
