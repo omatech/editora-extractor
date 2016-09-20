9e7131745092534098a2f900c7bc77be14515577
Agusti Pons <apons@omatech.com>
2016-09-20 12:16:39 +0200

    order al instancelist

--- src/Omatech/Editora/Extractor/EditoraData.php
+++ src/Omatech/Editora/Extractor/EditoraData.php
@@ -802,6 +802,7 @@ $order_sql="";
 				}
 
 */
+
 				//$sql="select i.id
 				$sql="select i.*, c.name class_name, c.tag class_tag, i.key_fields nom_intern, i.update_date, ifnull(unix_timestamp(i.update_date),0) update_timestamp  
 				from omp_instances i
