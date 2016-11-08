160a0a1681b4347ec3374eb0343ece4ef87688c4
Agusti Pons <apons@omatech.com>
2016-11-08 07:51:59 +0100

    graphqlpreprocessor in extractor package

--- src/Omatech/Editora/Extractor/GraphQLPreprocessor.php
+++ src/Omatech/Editora/Extractor/GraphQLPreprocessor.php
@@ -6,11 +6,24 @@ class GraphQLPreprocessor {
 		
     public static function generate($query, $end = true, $counter = 1) {
        $graphql = "";
-
+			 
         if(isset($query['type']) && $query['type'] === 'instance') {
             $graphql = '
                 query FetchGraphQuery ($id:Int, $lang:String, $debug:Boolean, $preview:Boolean) {
                     instance(id: $id, lang: $lang, debug: $debug, preview: $preview) {id nom_intern link class_id class_tag class_name all_values {atri_tag text_val num_val}';
+        }
+        if(isset($query['type']) && $query['type'] === 'class') {
+            $graphql = '
+                query FetchGraphQuery ($class_id:Int, $debug:Boolean, $lang:String, $preview:Boolean) {
+                    class(class_id: $class_id, lang: $lang, debug: $debug, preview: $preview) 
+										{
+										  class_id tag
+											instances {
+													id nom_intern link publishing_begins status creation_date class_name class_tag class_id update_timestamp
+													all_values (lang: $lang) {atri_tag text_val num_val}
+											}
+								';
+						  
         }
         if(isset($query['relations'])) {
             foreach ($query['relations'] as $key => $value) {
@@ -41,9 +54,15 @@ class GraphQLPreprocessor {
             }
         }
 
-        $graphql .= "}";
+        $graphql .= "\n}";
+				
+				if(isset($query['type']) && $query['type'] === 'class') {
+				// close extra bracket in case of class
+						$graphql .= "\n}";		
+				}
+				
         if($end == true) {
-            $graphql .= "}";
+            $graphql .= "\n}";
             $graphql = trim($graphql);
         }
         return $graphql;
