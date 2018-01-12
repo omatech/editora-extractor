15565ea9e92fafa6944b1a89af0da0f7ec56fabc
apons <apons@APONS-HP-EMO>
2018-01-12 10:24:04 +0100

    bug same attribute in different languages

---
+++ samples/launch10_bug_same_name_attribute.php
@@ -0,0 +1,103 @@
+<?php
+
+$autoload_location=__DIR__.'/../vendor/autoload.php';
+if (!is_file($autoload_location)) $autoload_location='../../../autoload.php';	
+
+require_once $autoload_location;
+require_once __DIR__.'/../conf/config.php';
+require_once __DIR__.'/../conf/bootstrap.php';
+
+use Omatech\Editora\Extractor\Extractor;
+use Omatech\Editora\Extractor\GraphQLPreprocessor;
+
+
+
+$params = [
+    'id'       => 441,
+    'preview'  => true,
+    'debug'    => true
+];
+
+$magic = [
+    "type"      => "instance",
+    "relations" => [
+        "Page_blocks"=>[
+            "alias"=>"blocks",
+            "relations" =>[
+                "Grouplinksfiles_fileslinks"=>[
+                    "relations" =>[
+                        "Link_destinationpages"
+                    ]
+
+                ],
+                "Listiconelement_Iconelements"=>[
+                    "relations"=>[
+                        "Iconelement_destinationpages"=>[
+                            "relations" =>[
+                                "Link_destinationpages"
+                            ]
+                        ],
+                        "Iconelement_filesdestinationpage"
+                    ]
+                ],
+                "Banner_filesdestinationpage" =>[
+                    "relations" =>[
+                        "Link_destinationpages"
+                    ]
+                ],
+                "Groupblockjob_Blockjoboutstanding"=>[
+                    "relations"=>[
+                        "Blockjob_destinationpages"=>[
+                            "relations" =>[
+                                "Link_destinationpages"
+                            ]
+                        ]
+                    ]
+                ],
+                "Groupblockjob_Blockjob"=>[
+                    "relations"=>[
+                        "Blockjob_destinationpages"=>[
+                            "relations" =>[
+                                "Link_destinationpages"
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ],
+        "Subpages",
+        //Padre
+        "Section_Pages"=>[
+            "limit"=>1,
+            'filter'=>'fields:title|niceurl',
+        ]
+    ]
+];
+
+
+
+$query=GraphQLPreprocessor::generate($magic);
+echo $query;
+
+
+$extractor=new Extractor($conn);
+$res=$extractor->extract($query, $params);
+
+if ($params['debug'])
+{
+		echo "DEBUG\n";
+		echo $extractor->debug_messages;
+}
+
+if ($res)
+{
+		echo "HA FUNCIONAT!!!";
+		print_r($res);
+}
+
+$res=$extractor->extract($query, $params, "json");
+if ($res)
+{
+		echo "HA FUNCIONAT!!!";
+		echo ($res);
+}
